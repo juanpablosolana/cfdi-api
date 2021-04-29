6fe26e18093d7096cf1db5011c75a372cccf2759
Juan Pablo Solana Ortiz <juanpablosolana@gmail.com>
2021-04-29 14:28:25 -0500

    v2.1
    Now the api is in /api/v1 and the root make a txt

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const cors = require('cors')
 const CfdiToJson = require('cfdi-to-json');
 const fs = require('fs');
 const path = require('path');
+
 let cfdi=[];
 let cfdiJson=[];
+let findId;
 const app = express();
 
 app.use(cors())
@@ -23,10 +25,29 @@ fs.readdir('./',  function (err, archivos) {
   cfdi.forEach(element => {
     cfdiJson.push(jsonCfdi = CfdiToJson.parse({ path: element }));
   })
+  findId = cfdiJson.filter(valor => valor.impuestos!==undefined)
 });
 
 app.get('/',  (request, response)=>{
-  response.send(cfdiJson.map(() => cfdiJson))
+  response.setHeader("Content-disposition", "attachment; filename=CFDINovalan.txt");
+  response.setHeader("Content-type", "text/plain");
+  response.charset = "UTF-8";
+
+  findId.map((cfdi)=>{
+    // console.log(
+    response.write(`${cfdi.version}|${cfdi.serie}|${cfdi.folio}|${cfdi.fecha}|${cfdi.formaPago}|${cfdi.subTotal}|${cfdi.descuento}|${cfdi.tipoCambio}|${cfdi.moneda}|${cfdi.total}|${cfdi.tipoDeComprobante}|${cfdi.metodoPago}|${cfdi.emisor.rfc}|${cfdi.emisor.nombre}|${cfdi.emisor.regimenFiscal}|${cfdi.receptor.rfc}|${cfdi.receptor.nombre}||||||||||||Ident Folio|${cfdi.timbreFiscal.uuid}|${cfdi.timbreFiscal.uuid}.xml|S - Comprobante obtenido satisfactoriamente. |Vigente|
+${cfdi.conceptos.map((element, index) => {
+  return `${cfdi.version}|${cfdi.serie}|${cfdi.folio}|${cfdi.fecha}|${cfdi.formaPago}|${cfdi.subTotal}|${cfdi.descuento}|${cfdi.tipoCambio}|${cfdi.moneda}|${cfdi.total}|${cfdi.tipoDeComprobante}|${cfdi.metodoPago}|${cfdi.emisor.rfc}|${cfdi.emisor.nombre}|${cfdi.emisor.regimenFiscal}|${cfdi.receptor.rfc}|${cfdi.receptor.nombre}|${cfdi.conceptos[index].cantidad}|${cfdi.conceptos[index].claveUnidad}|${cfdi.conceptos[index].claveProdServ}|${cfdi.conceptos[index].descripcion.replace(/\s+/g, ' ')}|${cfdi.conceptos[index].valorUnitario}|${cfdi.conceptos[index].importe}|||||||||||`}).join('\n')}
+${cfdi.version}|${cfdi.serie}|${cfdi.folio}|${cfdi.fecha}|${cfdi.formaPago}|${cfdi.subTotal}|${cfdi.descuento}|${cfdi.tipoCambio}|${cfdi.moneda}|${cfdi.total}|${cfdi.tipoDeComprobante}|${cfdi.metodoPago}||||${cfdi.receptor.rfc}|${cfdi.receptor.nombre}|||||||IVA|16|${cfdi.impuestos.totalImpuestosTrasladados}||||||||
+`)
+  })
+
+  response.end();
+})
+
+
+app.get('/api/v1', (request, response)=>{
+  response.send(findId)
 })
 
 const PORT = process.env.PORT || 3000;
